cdb9c489d3c21d8b475105e6ba3270390e8713f1
marius-mather <marius.mather@sydney.edu.au>
2025-08-14 16:31:30 +1000

    Add MDX to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+import createMDX from '@next/mdx'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
+};
 
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
 
-module.exports = nextConfig;
