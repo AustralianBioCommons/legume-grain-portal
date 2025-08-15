f78473759448c10ccd27265a07c3025545c23a1a
marius-mather <marius.mather@sydney.edu.au>
2025-08-15 13:58:02 +1000

    Add assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import createMDX from "@next/mdx";
 const nextConfig = {
   output: "export",
   basePath: "/legume-grain-portal",
+  assetPrefix: "/legume-grain-portal/",
   pageExtensions: ["md", "mdx", "ts", "tsx"],
 };
 
