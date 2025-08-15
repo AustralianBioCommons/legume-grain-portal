2b2d42cd0357bf25c79696bfbdd2dcc8ad19f6f2
marius-mather <marius.mather@sydney.edu.au>
2025-08-15 13:14:50 +1000

    Tweak header image

--- components/HeaderImage.tsx
+++ components/HeaderImage.tsx
@@ -14,13 +14,14 @@ export default function HeaderImage() {
         className="z-0 h-full w-full object-cover"
         radius={"none"}
         loading="eager"
+        priority
         disableSkeleton
         as={NextImage}
         width={2000}
         height={300}
       />
       <CardHeader className="absolute z-10 top-1/2 -translate-y-1/2 left-2 right-2 bg-white/60 w-fit p-4 rounded-lg">
-        <h2 className="text-4xl font-bold text-blue-900">
+        <h2 className="text-4xl font-bold text-slate-900">
           The Legume Grain 'Omics Initiative
         </h2>
       </CardHeader>
