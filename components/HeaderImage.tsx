6f09724bea6691df2187e0cafefed4a1ef5005db
marius-mather <marius.mather@sydney.edu.au>
2025-08-18 09:44:38 +1000

    Simplify header: don't use a card

--- components/HeaderImage.tsx
+++ components/HeaderImage.tsx
@@ -1,13 +1,9 @@
-import { Card, CardHeader } from "@heroui/card";
 import { Image } from "@heroui/image";
 import { addBasePath } from "next/dist/client/add-base-path";
 
 export default function HeaderImage() {
   return (
-    <Card
-      radius={"none"}
-      className="w-full overflow-hidden h-64 md:h-80 flex flex-col items-center"
-    >
+    <div className="relative w-full overflow-hidden h-64 md:h-80 flex flex-col items-center">
       <Image
         src={addBasePath("/img/Header-Opt1.jpg")}
         alt="Header image"
@@ -19,11 +15,11 @@ export default function HeaderImage() {
         width={2000}
         height={320}
       />
-      <CardHeader className="absolute backdrop-blur-xs justify-center md:max-w-3xl top-1/2 -translate-y-1/2 z-10 bg-white/40 p-4 md:rounded-lg">
+      <div className="absolute backdrop-blur-xs justify-center md:max-w-3xl top-1/2 -translate-y-1/2 z-10 bg-white/40 p-4 md:rounded-lg">
         <h2 className="text-center text-4xl font-extrabold text-green-950">
           The Legume Grain 'Omics Initiative
         </h2>
-      </CardHeader>
-    </Card>
+      </div>
+    </div>
   );
 }
